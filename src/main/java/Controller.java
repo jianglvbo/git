0b7b5f0f0e70796f2a10a110aac022e3586651f5
jianglvbo <49331439+jianglvbo@users.noreply.github.com>
2024-06-19 23:14:27 +0800

    泉州 20

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,9 @@
 public class Controller {
     private  void getAge(){
-        System.out.println("18");
+        System.out.println("20");
     }
 
     private void getAddress(){
-        System.out.println("厦门");
+        System.out.println("泉州");
     }
 }
